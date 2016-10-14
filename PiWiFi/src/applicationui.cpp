c89f3615bdb29e050600df4f893d5375090855b2
pjamison1 <pij421uk@googlemail.com>
2016-10-14 17:50:49 +0100

    Update applicationui.cpp

--- PiWiFi/src/applicationui.cpp
+++ PiWiFi/src/applicationui.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * This line added to cloned source
+ * This line added via GitHub Edit facility online.
  */
 
 #include "applicationui.hpp"
